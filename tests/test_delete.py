1818383266f5ad90d5d9e97a5488e81bff214436
Martin Panter <vadmium  gmailcom>
2017-03-04 03:46:13 +0000

    Remove merged old Python compatibility code

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -1,11 +1,13 @@
-import os, os.path
+from contextlib import redirect_stdout
+from io import StringIO
+import os.path
 from quilt.db import Series
 from quilt.patch import Patch
-from quilt.utils import TmpDirectory
-from six.moves import cStringIO
-import sys
+from tempfile import TemporaryDirectory
+from types import SimpleNamespace
+from unittest import mock
 
-from helpers import QuiltTest, tmp_mapping
+from helpers import QuiltTest
 
 from quilt.delete import Delete
 from quilt.cli.delete import DeleteCommand
@@ -14,33 +16,29 @@ class Test(QuiltTest):
 
     def test_next_first(self):
         """ Delete the next patch with only unapplied patches """
-        with TmpDirectory() as dir:
-            patches = Series(dir.get_name())
+        with TemporaryDirectory() as dir:
+            patches = Series(dir)
             patches.add_patch(Patch("patch"))
             patches.save()
-            cmd = Delete(dir.get_name(), dir.get_name(), patches.dirname)
+            cmd = Delete(dir, dir, patches.dirname)
             cmd.delete_next()
             patches.read()
             self.assertTrue(patches.is_empty())
     
     def test_no_backup(self):
         """ Remove a patch without leaving a backup """
-        with TmpDirectory() as dir:
-            patches = Series(dir.get_name())
+        with TemporaryDirectory() as dir:
+            patches = Series(dir)
             patches.add_patch(Patch("patch"))
             patches.save()
-            patch = os.path.join(dir.get_name(), "patch")
+            patch = os.path.join(dir, "patch")
             with open(patch, "wb"):
                 pass
-            class options:
-                next = True
-                remove = True
-                backup = False
-            with tmp_mapping(os.environ) as env, \
-                    tmp_mapping(vars(sys)) as tmp_sys:
-                env.set("QUILT_PATCHES", patches.dirname)
-                env.set("QUILT_PC", dir.get_name())
-                tmp_sys.set("stdout", cStringIO())
+            options = SimpleNamespace(next=True, remove=True, backup=False)
+            with mock.patch.dict("os.environ", (
+                ("QUILT_PATCHES", patches.dirname),
+                ("QUILT_PC", dir),
+            )), redirect_stdout(StringIO()):
                 DeleteCommand().run(options, [])
             self.assertFalse(os.path.exists(patch))
             self.assertFalse(os.path.exists(patch + "~"))
