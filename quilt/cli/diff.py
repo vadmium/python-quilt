72ee53a396e5cb7adf9568a2022609c91fea6af3
Martin Panter <vadmium  gmailcom>
2018-09-05 11:05:32 +1000

    diff -z

--- quilt/cli/diff.py
+++ quilt/cli/diff.py
@@ -1,20 +1,56 @@
+import os.path
 import sys
 
 from .meta import Command
+from .parser import OptionArgument
 from .. import db
-from ..patch import _generate_patch
+from .. import patch
+from ..utils import File, Directory
+from ..utils import TmpDirectory
 
 class diff(Command):
 
     name = "diff"
     help = "Show the diff that would be refreshed for the topmost patch."
     
+    z = OptionArgument(action="store_true", help="""unrefreshed changes only,
+        relative to the topmost patch""")
+    
     def run(self, args):
         applied = self.get_db()
-        patch = db._get_top(applied)
+        target_patch = db._get_top(applied)
         if sys.version_info.major >= 3:
             out = sys.stdout.buffer
         else:
             out = sys.stdout
-        _generate_patch(self.get_cwd(),
-            self.get_db(), self.get_patches_dir(), out, patch)
+        cwd = self.get_cwd()
+        if args.z:
+            # Apply original patch in temporary tree; then diff from that
+            with TmpDirectory(prefix="pquilt-") as base:
+                # Copy patched files from .pc/ to base
+                initial = Directory(self.get_pc_dir())
+                initial += target_patch.get_name()
+                files = initial.files()
+                for name in files:
+                    if name == ".timestamp":
+                        continue
+                    file = File(name)
+                    (initial + file).copy(base + file)
+                target_patch.run(force=True, cwd=None,
+                    patch_dir=os.path.abspath(self.get_patches_dir()),
+                    work_dir=base, no_backup_if_mismatch=True,
+                    quiet=True,
+                )
+                for name in files:
+                    if name == ".timestamp":
+                        continue
+                    file = File(name)
+                    old = base + file
+                    [olabel, nlabel, index] = patch._get_labels(name,
+                        old, file, cwd=cwd)
+                    patch._write_index(out, index)
+                    out.flush()
+                    patch.Diff(old, file).run(cwd, olabel, nlabel)
+        else:
+            patch._generate_patch(cwd,
+                self.get_db(), self.get_patches_dir(), out, target_patch)
